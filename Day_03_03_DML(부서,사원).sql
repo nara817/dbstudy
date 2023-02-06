2bdcb4cfb03d356582506dbe2816d18b60e71181
nara817 <knr817@naver.com>
2023-02-06 14:45:48 +0900

    2023-02-06

---
+++ Day_03_03_DML(부서,사원).sql
@@ -0,0 +1,15 @@
+/*
+    DML
+    1. Data manipulation Language
+    2. 데이터 조작어
+    3. 데이터(행, Row)를 삽입, 수정, 삭제할 때 사용하는 언어이다.
+    4. DML 사용 후에는 COMMIT 또는 ROLLBACK 처리를 해야한다.★
+    5. 종류
+        1) 삽입 : INSERT INTO VALUES
+        2) 수정 : UPDATE SET WHERE
+        3) 삭제 : DELETE FROM WHERE
+     - 트랜잭션이 필요한 SQL
+        COMMIT 필요 - INSERT,UPDATE,DELETE 
+*/
+
+-- 참고. DML을 INSERT,UPDATE,DELETE + SELECT로 보는 경우도 있다.
